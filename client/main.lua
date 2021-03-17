217bc628a5d77d36a3cc1f86e707021aa6612d2f
czsquizer <56487372+czsquizer@users.noreply.github.com>
2021-03-17 19:23:15 +0100

    add compatibility for onesync infinity

--- client/main.lua
+++ client/main.lua
@@ -3,13 +3,15 @@ AddEventHandler('esx_rpchat:sendProximityMessage', function(playerId, title, mes
 	local player = PlayerId()
 	local target = GetPlayerFromServerId(playerId)
 
-	local playerPed = PlayerPedId()
-	local targetPed = GetPlayerPed(target)
-	local playerCoords = GetEntityCoords(playerPed)
-	local targetCoords = GetEntityCoords(targetPed)
+	if target ~= -1 then
+		local playerPed = PlayerPedId()
+		local targetPed = GetPlayerPed(target)
+		local playerCoords = GetEntityCoords(playerPed)
+		local targetCoords = GetEntityCoords(targetPed)
 
-	if target == player or #(playerCoords - targetCoords) < 20 then
-		TriggerEvent('chat:addMessage', {args = {title, message}, color = color})
+		if target == player or #(playerCoords - targetCoords) < 20 then
+			TriggerEvent('chat:addMessage', {args = {title, message}, color = color})
+		end
 	end
 end)
 
